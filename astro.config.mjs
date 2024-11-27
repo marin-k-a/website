fce3b6de4487066ec29ae6fbbe2066d88d9f67cc
Marina <mz.zaytseva@gmail.com>
2024-11-27 12:06:49 +0100

    Changing the folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://marin-k-a.com',
-    outDir: 'dist',
+    outDir: 'docs',
     base: '/website/',
     integrations: [
         mdx(),
